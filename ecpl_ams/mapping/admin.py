78f260ff8cca0820bed0945eb07bd19ceb584ba5
Ibrahim Khan <ibrahim7353khan@gmail.com>
2022-04-12 13:28:09 +0530

    Added Mapping App

---
+++ ecpl_ams/mapping/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+class ProfileSearch(admin.ModelAdmin):
+    search_fields = ('emp_name', 'emp_id', "emp_desi")
+    list_display = ('emp_name', 'emp_id', 'emp_desi', 'emp_process', "emp_rm1", "emp_rm2", "emp_rm3")
+
+
+admin.site.register(Profile, ProfileSearch)
---
+++ ecpl_ams/mapping/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    emp_id = models.CharField(max_length=100)
+    emp_name = models.CharField(max_length=200)
+    emp_desi = models.CharField(max_length=200)
+    emp_rm1 = models.CharField(max_length=200)
+    emp_rm1_id = models.CharField(max_length=30)
+    emp_rm2 = models.CharField(max_length=200)
+    emp_rm2_id = models.CharField(max_length=30)
+    emp_rm3 = models.CharField(max_length=200)
+    emp_rm3_id = models.CharField(max_length=30)
+    emp_process = models.CharField(max_length=200)
+    emp_process_id = models.CharField(max_length=10)
+    pc = models.BooleanField(default=False)
+    img = models.ImageField(upload_to='user_images', default="user_images/user.png")
+    doj = models.DateField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    on_id = models.IntegerField(null=True, blank=True)
+    agent_status = models.CharField(max_length=30, default='Active')
+
+    def __str__(self):
+        return self.emp_name
