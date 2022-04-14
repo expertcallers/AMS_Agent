67997ea3ad4301481b135fc2ca61bb172625b6e4
Ibrahim Khan <ibrahim7353khan@gmail.com>
2022-04-14 17:24:50 +0530

    Corrected some minor issues

--- ecpl_ams/ecpl_ams/urls.py
+++ ecpl_ams/ecpl_ams/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ams/', include('ams.urls')),
+    path('accounts/', include('ams.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
