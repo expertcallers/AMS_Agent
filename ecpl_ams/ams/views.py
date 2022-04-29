a6d9dc68bf5f5556580ec0fd779d04ebf7161ab1
Ibrahim Khan <ibrahim7353khan@gmail.com>
2022-04-29 13:19:04 +0530

    Corrected Error in Apply Leave

--- ecpl_ams/ams/views.py
+++ ecpl_ams/ams/views.py
@@ -196,6 +196,17 @@ def applyLeave(request):  # Test1
             elif leave_type == 'SL':
                 leave_balance.sl_balance -= int(no_days)
                 leave_balance.save()
+
+            leave_history = leaveHistory()
+            leave_history.leave_type = leave_type
+            leave_history.transaction = 'Leave Applied (ID: '+str(e.id)+')'
+            leave_history.date = date.today()
+            leave_history.no_days = int(no_days)
+            leave_history.emp_id = emp_id
+            pl = EmployeeLeaveBalance.objects.get(emp_id=emp_id).pl_balance
+            sl = EmployeeLeaveBalance.objects.get(emp_id=emp_id).sl_balance
+            leave_history.total = pl + sl
+            leave_history.save()
             return redirect('/ams/ams-apply_leave')
     else:
         emp_id = request.user.profile.emp_id
@@ -218,7 +229,7 @@ def applyLeave(request):  # Test1
         except EmployeeLeaveBalance.DoesNotExist:
             leave_balance = {'sl_balance': 0, 'pl_balance': 0}
         leave_his = leaveHistory.objects.filter(emp_id=emp_id).values('date', 'transaction',
-                                                                      'leave_type', 'total').annotate(
+                                                                      'leave_type', 'total', 'id').annotate(
             no_days=Sum('no_days'))
         data = {'emp': emp, 'leave': leave, 'leave_balance': leave_balance, 'probation': probation,
                 'leave_his': leave_his}
@@ -285,6 +296,18 @@ def applyEscalation(request):  # Test1
         else:
             a.sl_balance = a.sl_balance - no_days
         a.save()
+
+        leave_history = leaveHistory()
+        leave_history.leave_type = e.leave_type
+        leave_history.transaction = 'Applied for Escalation, Leave applied on: ' + str(e.applied_date)+' (ID: '+str(e.id)+')'
+        leave_history.date = date.today()
+        leave_history.no_days = int(no_days)
+        leave_history.emp_id = emp_id
+        pl = EmployeeLeaveBalance.objects.get(emp_id=emp_id).pl_balance
+        sl = EmployeeLeaveBalance.objects.get(emp_id=emp_id).sl_balance
+        leave_history.total = pl + sl
+        leave_history.save()
+
         messages.info(request, "Leave Escalated Successfully!")
         return redirect('/ams/ams-apply_leave')
     else:
